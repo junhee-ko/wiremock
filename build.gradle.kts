c152ebc13104efbe6fdbb67965ff9d1c42d01d1f
junhee-ko <junheee.ko@gmail.com>
2022-06-05 19:46:16 +0900

    Add dependency: spring-boot-starter-web, wiremock

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,12 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.2")
 }
 
 tasks.withType<KotlinCompile> {
